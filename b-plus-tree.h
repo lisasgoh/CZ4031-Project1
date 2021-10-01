892148fac7cb7c398c56c9e44b6257be5ef03e00
chew0205 <50271734+chew0205@users.noreply.github.com>
2021-10-02 00:55:50 +0800

    memory address changes

--- b-plus-tree.h
+++ b-plus-tree.h
@@ -7,64 +7,64 @@
 using namespace std;
 
 struct keys_struct {
-  float key_value;
-  vector<void *> add_vect;
+    float key_value;
+    vector<void *> add_vect;
 };
 
 class BPTree; // self explanatory classes
 class Node {
-  bool IS_LEAF;
-  keys_struct *key;
-  int size;
-  Node **ptr;
-  friend class BPTree;
+    bool IS_LEAF;
+    keys_struct *key;
+    int size;
+    Node **ptr;
+    friend class BPTree;
 
 public:
-  Node();
+    Node();
 };
 
 class BPTree {
-  Node *root;
-  void insertInternal(keys_struct x, Node *cursor, Node *child);
-  int removeInternal(keys_struct x, Node *cursor, Node *child);
-  Node *findParent(Node *cursor, Node *child);
-  int numNode = 0;
+    Node *root;
+    void insertInternal(keys_struct x, Node *cursor, Node *child);
+    void removeInternal(keys_struct x, Node *cursor, Node *child);
+    Node *findParent(Node *cursor, Node *child);
+    int numNode = 0;
 
 public:
-  BPTree();
-  Node *search(float x, bool flag, bool printer);
-  int height(Node *cursor);
-  void insert(keys_struct x);
-  int remove(keys_struct x);
-  int display(Node *cursor, int nodecount, bool first);
-  Node *getRoot();
-  int getMax();
-  int getNumNode() { return numNode; }
-  //    void cleanUp(Node* cursor)
-  //    {
-  //        //clean up logic
-  //        if(cursor!=NULL)
-  //        {
-  //            if(cursor->IS_LEAF != true)
-  //            {
-  //                for(int i = 0; i < cursor->size+1; i++)
-  //                {
-  //                    cleanUp(cursor->ptr[i]);
-  //                }
-  //            }
-  //            for(int i = 0; i < cursor->size; i++)
-  //            {
-  //                cout<<"Deleted key from memory: "<<cursor->key[i].key_value
-  //                << " " << cursor->key[i].add_vect[0] <<"\n";
-  //            }
-  //            delete[] cursor->key;
-  //            delete[] cursor->ptr;
-  //            delete cursor;
-  //        }
-  //    }
-  //    ~BPTree()
-  //    {
-  //        //calling cleanUp routine
-  //        cleanUp(root);
-  //    }
+    BPTree();
+    Node *search(float x, bool flag, bool printer);
+    int height(Node *cursor);
+    void insert(keys_struct x);
+    void remove(keys_struct x);
+    int display(Node *cursor, int nodecount, bool first);
+    Node *getRoot();
+    int getMax();
+    int getNumNode() { return numNode; }
+    //    void cleanUp(Node* cursor)
+    //    {
+    //        //clean up logic
+    //        if(cursor!=NULL)
+    //        {
+    //            if(cursor->IS_LEAF != true)
+    //            {
+    //                for(int i = 0; i < cursor->size+1; i++)
+    //                {
+    //                    cleanUp(cursor->ptr[i]);
+    //                }
+    //            }
+    //            for(int i = 0; i < cursor->size; i++)
+    //            {
+    //                cout<<"Deleted key from memory: "<<cursor->key[i].key_value
+    //                << " " << cursor->key[i].add_vect[0] <<"\n";
+    //            }
+    //            delete[] cursor->key;
+    //            delete[] cursor->ptr;
+    //            delete cursor;
+    //        }
+    //    }
+    //    ~BPTree()
+    //    {
+    //        //calling cleanUp routine
+    //        cleanUp(root);
+    //    }
 };
